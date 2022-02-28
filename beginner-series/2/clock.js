e8823ec22d246f99daa1b474854dc3fe99577468
Amir <amirhatam136@gmail.com>
2022-02-28 17:24:00 +0100

    init beginner ex-3

--- beginner-series/2/clock.js
+++ beginner-series/2/clock.js
@@ -1,7 +1,11 @@
-
-
-
 //Best practise
 const milliseconds = (h, m, s) => h * 60 * 60 * 1000 + m * 60 * 1000 + 1000 * s
 
-console.log(milliseconds(0, 1, 30));
+// console.log(milliseconds(0, 1, 30));
+
+//Another Best practise
+function past(h, m, s) {
+    return ((h * 3600) + (m * 60) + s) * 1000;
+}
+
+console.log(past(0, 1, 30));
--- beginner-series/README.md
+++ beginner-series/README.md
@@ -3,3 +3,5 @@
 ## 1-School Paperwork
 
 ## 2-Clock
+
+## 3-Sum of Numbers
