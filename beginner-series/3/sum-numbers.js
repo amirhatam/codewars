915a61b82b897aa4fabcc7269c123e7c76d8efce
Amir <amirhatam136@gmail.com>
2022-02-28 18:10:02 +0100

    beginner ex-3, sum numbers, Seconde solution

--- beginner-series/3/sum-numbers.js
+++ beginner-series/3/sum-numbers.js
@@ -4,4 +4,12 @@ const sum = (a, b) => {
         max = Math.max(a, b);
     return (max - min + 1) * (min + max) / 2;
 }
-console.log(sum(1, 0));
+console.log(sum(0, 2));
+
+//Seconde solution
+const getSum = (a, b) => {
+    if (a == b) return a;
+    else if (a < b) return a + getSum(a + 1, b);
+    else return a + getSum(a - 1, b)
+}
+console.log(getSum(0, 2));
