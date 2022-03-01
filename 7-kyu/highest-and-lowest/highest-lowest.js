d3dcc00e7f72d1e83f61c347368a33ee97f1260b
Amir <amirhatam136@gmail.com>
2022-03-01 23:27:56 +0100

    Highest and Lowest,best practices

--- 7-kyu/highest-and-lowest/highest-lowest.js
+++ 7-kyu/highest-and-lowest/highest-lowest.js
@@ -4,4 +4,10 @@ function highAndLow(numbers) {
     return `${Math.max(...nums)} ${Math.min(...nums)}`;
 }
 
+
+//Best practices
+function highAndLow(numbers) {
+    numbers = numbers.split(' ').map(Number);
+    return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
+}
 console.log(highAndLow("1 9 3 4 -5"));
