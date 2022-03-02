f3847c97626e4a15cadf22b95f98b04922990eba
Amir <amirhatam136@gmail.com>
2022-03-02 16:39:38 +0100

    Don't give me five, One solution with method includes

--- 7-kyu/don't-give-me-five/don't-give-me-five.js
+++ 7-kyu/don't-give-me-five/don't-give-me-five.js
@@ -1,4 +1,4 @@
-// One solution
+// One solution with method RegExp.test
 const countNum = (a, b) => {
     let res = 0
     for (let i = a; i <= b; i++) {
@@ -8,3 +8,14 @@ const countNum = (a, b) => {
     return res
 }
 console.log(countNum(4, 17));
+
+// One solution with method includes
+function dontGiveMeFive(start, end) {
+    let res = [];
+    for (let i = start; i <= end; i++) {
+        if (!i.toString().includes('5')) res.push(i);
+    }
+    return res.length;
+}
+
+console.log(dontGiveMeFive(4, 17));
