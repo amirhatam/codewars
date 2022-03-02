4d2bf70a44808c086ac19b9603e2900a4de8df1c
Amir <amirhatam136@gmail.com>
2022-03-02 16:05:09 +0100

    Number Pairs, one solution with Math.max

--- 7-kyu/number-pairs/number-pairs.js
+++ 7-kyu/number-pairs/number-pairs.js
@@ -1,3 +1,8 @@
+let arr1 = [13, 64, 15, 17, 88];
+let arr2 = [23, 14, 53, 17, 80];
+
+
+//One solution without Math.max
 const pairsNum = (a, b) => {
     let res = []
     for (let i = 0; i < a.length; i++) {
@@ -8,7 +13,17 @@ const pairsNum = (a, b) => {
     }
     return res
 }
-let arr1 = [13, 64, 15, 17, 88];
-let arr2 = [23, 14, 53, 17, 80];
 
 console.log(pairsNum(arr1, arr2));
+
+//Math.max solution
+
+function getLargerNumbers(a, b) {
+    let newArray = [];
+    for (i = 0; i < a.length; i++)
+        newArray.push(Math.max(a[i], b[i]));
+
+    return newArray;
+}
+
+console.log(getLargerNumbers(arr1, arr2));
