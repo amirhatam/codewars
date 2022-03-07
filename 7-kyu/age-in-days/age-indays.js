d2bcd515711b9f787d5a0f6f2feaa08838224fd6
Amir <amirhatam136@gmail.com>
2022-03-07 20:35:57 +0100

    Is the date today, init

--- 7-kyu/age-in-days/age-indays.js
+++ 7-kyu/age-in-days/age-indays.js
@@ -5,6 +5,9 @@ const ageCalculator = (y, m, d) => {
     let month = new Date().getMonth() + 1
     let day = new Date().getDate()
 
+    let arrMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    let arrMonthsD = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
     let resDay = 0
     let resMonth = 0
     let resYear = 0
@@ -18,7 +21,7 @@ const ageCalculator = (y, m, d) => {
     }
 
     if (month < m) {
-        resMonth = (month + 12) - m
+        resMonth = (resMonth + 12) - m
         resYear = (year - 1)
     } else {
         resMonth = month - m
@@ -26,22 +29,16 @@ const ageCalculator = (y, m, d) => {
     }
     resYear -= y
 
-    // console.log("resYear", resYear);
-    // console.log("y", y);
 
-    const age = [resYear, "Years, ", resMonth, "Months, ", resDay, "Days"]
+
+    console.log(resYear, "Years, ", resMonth, "Months, ", resDay, "Days");
     const ageInDays = (resYear * 365) + (resMonth * 31) + resDay
 
 
     const res = `You are ${ageInDays} days old`
     return res
-
 }
 
-console.log(ageCalculator(2007, 11, 27));
+console.log(ageCalculator(2007, 04, 28));
+// console.log(ageCalculator(1986, 10, 10));
 // ageCalculator(1986, 10, 10)
-
-
-
-
-
