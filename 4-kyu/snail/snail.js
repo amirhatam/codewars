3538b8f608f0e64cbe997177c8ee2d82f551acdc
Amir <amirhatam136@gmail.com>
2022-03-07 23:19:52 +0100

    snail,two Best Practice with While solution

--- 4-kyu/snail/snail.js
+++ 4-kyu/snail/snail.js
@@ -13,7 +13,7 @@ const snail = () => {
     return newArr.sort((a, b) => a - b);
 }
 
-console.log(snail());
+// console.log(snail());
 
 // Another solution
 var snail2 = function (array) {
@@ -36,4 +36,38 @@ var snail2 = function (array) {
     return r;
 };
 
-console.log(snail2(array));
+// console.log(snail2(array));
+
+
+// Best Practice with While solution
+const snailWhile = function (array) {
+    var result;
+    while (array.length) {
+        // Steal the first row.
+        result = (result ? result.concat(array.shift()) : array.shift());
+        // Steal the right items.
+        for (var i = 0; i < array.length; i++)
+            result.push(array[i].pop());
+        // Steal the bottom row.
+        result = result.concat((array.pop() || []).reverse());
+        // Steal the left items.
+        for (var i = array.length - 1; i >= 0; i--)
+            result.push(array[i].shift());
+    }
+    return result;
+}
+
+console.log(snailWhile(array));
+
+// Best Practice with While solution
+function snailWhile2(array) {
+    var vector = [];
+    while (array.length) {
+        vector.push(...array.shift());
+        array.map(row => vector.push(row.pop()));
+        array.reverse().map(row => row.reverse());
+    }
+    return vector;
+}
+
+// console.log(snailWhile2(array));
