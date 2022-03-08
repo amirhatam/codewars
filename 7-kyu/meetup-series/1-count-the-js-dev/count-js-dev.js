7d62d2ebd442682761862747aded5bbb67d81536
Amir <amirhatam136@gmail.com>
2022-03-08 08:31:20 +0100

    Coding Meetup, 1- count the js dev, with map

---
+++ 7-kyu/meetup-series/1-count-the-js-dev/count-js-dev.js
@@ -0,0 +1,23 @@
+var list1 = [
+    { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+    { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+    { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+    { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' },
+    { firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 19, language: 'HTML' },
+    { firstName: 'Lukas', lastName: 'R.', country: 'Austria', continent: 'Europe', age: 89, language: 'HTML' }
+];
+
+const findJsDev = () => {
+    // let res = []
+    let count = 0
+    list1.map(e => {
+        if (e.continent === 'Europe' && e.language === 'JavaScript') {
+            // res.push(e)
+            count++
+        }
+    })
+    return res
+}
+
+console.log(findJsDev());
+
