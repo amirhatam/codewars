7c61e892bf454242480d1317ede7818a926c43ad
Amir <amirhatam136@gmail.com>
2022-03-11 18:01:44 +0100

    edit folders name, One solution

---
+++ meetup-series/11-Find-average-age/11-Find-average-age.js
@@ -0,0 +1,17 @@
+var list = [
+    { firstName: 'Maria', lastName: 'Y.', country: 'Cyprus', continent: 'Europe', age: 30, language: 'Java' },
+    { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 70, language: 'Python' },
+];
+var list2 = [
+    { firstName: 'Noa', lastName: 'A.', country: 'Israel', continent: 'Asia', age: 20, language: 'Ruby' },
+    { firstName: 'Andrei', lastName: 'E.', country: 'Romania', continent: 'Europe', age: 21, language: 'C' },
+];
+
+//One solution
+const getAvrageAge = () => {
+    const average = arr => arr.reduce((a, b) => a + b, 0) / arr.length;
+    let ageList = list.map(e => e.age)
+    return Math.round(average(ageList))
+
+}
+console.log(getAvrageAge());
