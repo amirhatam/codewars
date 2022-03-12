655d79181e3252622ee55b8ce0810a17582aaf2f
Amir <amirhatam136@gmail.com>
2022-03-12 11:56:38 +0100

    12 - Find GitHub admins,One solution But Not accepted !!!

---
+++ meetup-series/12-find-admins/12-find-admins.js
@@ -0,0 +1,20 @@
+const list = [
+    { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' },
+    { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 49, language: 'Ruby', githubAdmin: 'no' },
+    { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'JavaScript', githubAdmin: 'yes' },
+    { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript', githubAdmin: 'no' }
+];
+
+//One solution But Not accepted !!!
+const findAdmin = (list, lang) => {
+    let newList = []
+    list.map(e => {
+        if (e.githubAdmin === 'yes' && e.language === 'JavaScript') {
+            newList.push(e)
+        }
+    })
+    return newList
+}
+
+console.log(findAdmin(list, 'JavaScript'));
+
