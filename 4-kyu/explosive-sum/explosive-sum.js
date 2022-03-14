82cedd4e8c7fe264f9e727fc13fbf6700b3a2509
Amir <amirhatam136@gmail.com>
2022-03-14 10:19:37 +0100

    Explosive Sum, Best Practice

--- 4-kyu/explosive-sum/explosive-sum.js
+++ 4-kyu/explosive-sum/explosive-sum.js
@@ -34,3 +34,21 @@ function sum(num) {
 }
 
 console.log(sum(5));
+
+
+//Best Practice
+var memo = [];
+
+function sumB(n, m = n) {
+    if (n == 0) return 1;
+    if (n < 0 || m == 0) return 0;
+    if (memo[n] && memo[n][m]) return memo[n][m];
+    let total = sum(n, m - 1) + sum(n - m, m);
+    if (!memo[n]) {
+        memo[n] = [];
+    }
+    memo[n][m] = total;
+    return total;
+}
+
+console.log(sumB(5));
