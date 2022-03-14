116d5a38569bd0c71a7ccded7e938ecacc21b7a7
Amir <amirhatam136@gmail.com>
2022-03-14 09:54:54 +0100

    Range Extraction,BEST Practices

--- 4-kyu/range-extraction/range-extraction.js
+++ 4-kyu/range-extraction/range-extraction.js
@@ -21,5 +21,16 @@ const solution = () => {
     return res.slice(0, -1);
 }
 
-console.log(solution());
+// console.log(solution());
 
+//BEST Practices
+function bestPractices() {
+    for (var i = 0; i < list.length; i++) {
+        var j = i;
+        while (list[j] - list[j + 1] == -1) j++;
+        if (j != i && j - i > 1) list.splice(i, j - i + 1, list[i] + '-' + list[j]);
+    }
+    return list.join();
+}
+
+console.log(bestPractices());
