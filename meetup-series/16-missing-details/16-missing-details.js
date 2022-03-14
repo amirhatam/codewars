da1c33074c882d70af3e74281bf62911c2718ddb
Amir <amirhatam136@gmail.com>
2022-03-14 11:26:10 +0100

    16 - Ask for missing details,One Solution

---
+++ meetup-series/16-missing-details/16-missing-details.js
@@ -0,0 +1,14 @@
+const list = [
+    { firstName: null, lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+    { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: null },
+    { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' }
+];
+
+//One Solution
+const askForMissingDetails = () => {
+    let newList = []
+    newList = list.filter(dev => Object.keys(dev).some(e => dev[e] === null))
+    return newList.map(dev => ({ ...dev, question: `Hi, could you please provide your ${Object.keys(dev).find(e => dev[e] === null)}.` }))
+}
+
+console.log(askForMissingDetails());
