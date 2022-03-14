668d33de03a85fb2ee58be29e110f072494c0f48
Amir <amirhatam136@gmail.com>
2022-03-14 09:52:54 +0100

    Range Extraction,One solution

---
+++ 4-kyu/range-extraction/range-extraction.js
@@ -0,0 +1,25 @@
+const list = ([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
+
+//One solution
+const solution = () => {
+    let res = "";
+    let isRange = false;
+
+    for (let i = 0; i < list.length; ++i) {
+        const j = list[i];
+
+        if (list[i + 1] !== j + 1) {
+            if (isRange) res += (list[i] === list[i - 2] + 2) ? "-" : ",";
+            res += j + ",";
+            isRange = false;
+        }
+        else if (!isRange) {
+            res += j;
+            isRange = true;
+        }
+    }
+    return res.slice(0, -1);
+}
+
+console.log(solution());
+
