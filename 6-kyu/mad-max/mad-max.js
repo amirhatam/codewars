561fc9555dc364a4324c2ca07703f4418580a3d8
Amir <amirhatam136@gmail.com>
2022-06-04 22:01:23 +0200

    Mad Max, best practices

--- 6-kyu/mad-max/mad-max.js
+++ 6-kyu/mad-max/mad-max.js
@@ -22,3 +22,15 @@ function max(array) {
 
 // console.log(max(myArray));
 max(myArray)
+
+// Best Solution
+function max(arr) {
+    if (!arr.length)
+        return -Infinity;
+
+    if (arr.length === 1)
+        return arr[0];
+
+    const [a, b] = [arr[0], max(arr.slice(1))];
+    return a < b ? b : a;
+}
