660109f24583266be2d2448fb46f46704783b2aa
Amir <amirhatam136@gmail.com>
2022-06-06 20:32:26 +0200

    Find the vowels, first solution

---
+++ 7-kyu/find-vowels/find-vowels.js
@@ -0,0 +1,25 @@
+// First Solution
+const vowelsIndex = (s) => {
+    const input = s.toLowerCase().split('')
+    let res = []
+
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] === 'a')
+            res.push(i + 1)
+        else if (input[i] === 'e')
+            res.push(i + 1)
+        else if (input[i] === 'i')
+            res.push(i + 1)
+        else if (input[i] === 'o')
+            res.push(i + 1)
+        else if (input[i] === 'u')
+            res.push(i + 1)
+        else if (input[i] === 'y')
+            res.push(i + 1)
+
+    }
+
+    return res
+}
+
+console.log(vowelsIndex('YoMama'));
