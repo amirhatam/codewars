e6a19b6070154550fe0eccacc5f4088829ee0743
Amir <amirhatam136@gmail.com>
2022-06-06 20:36:10 +0200

    Find the vowels, first solution, DRY Code

--- 7-kyu/find-vowels/find-vowels.js
+++ 7-kyu/find-vowels/find-vowels.js
@@ -4,19 +4,30 @@ const vowelsIndex = (s) => {
     let res = []
 
     for (let i = 0; i < input.length; i++) {
-        if (input[i] === 'a')
-            res.push(i + 1)
-        else if (input[i] === 'e')
-            res.push(i + 1)
-        else if (input[i] === 'i')
-            res.push(i + 1)
-        else if (input[i] === 'o')
-            res.push(i + 1)
-        else if (input[i] === 'u')
-            res.push(i + 1)
-        else if (input[i] === 'y')
-            res.push(i + 1)
+        // if (input[i] === 'a')
+        //     res.push(i + 1)
+        // else if (input[i] === 'e')
+        //     res.push(i + 1)
+        // else if (input[i] === 'i')
+        //     res.push(i + 1)
+        // else if (input[i] === 'o')
+        //     res.push(i + 1)
+        // else if (input[i] === 'u')
+        //     res.push(i + 1)
+        // else if (input[i] === 'y')
+        //     res.push(i + 1)
 
+        // DRY Code
+        if (
+            input[i] === "a" ||
+            input[i] === "e" ||
+            input[i] === "i" ||
+            input[i] === "o" ||
+            input[i] === "u" ||
+            input[i] === "y"
+        ) {
+            res.push(i + 1)
+        }
     }
 
     return res
