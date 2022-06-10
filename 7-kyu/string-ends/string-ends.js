2611cf03f6a4b6c71baad7c27a84edb0b8e65d08
Amir <amirhatam136@gmail.com>
2022-06-10 18:30:04 +0200

    String ends with, Best Practices But it's not correct !!!

--- 7-kyu/string-ends/string-ends.js
+++ 7-kyu/string-ends/string-ends.js
@@ -1,7 +1,16 @@
 //My Solution
 const sameLastStr = (a, b) => a.charAt(a.length - 1) === b.charAt(b.length - 1)
 
-
-
 // console.log(sameLastStr('abc', 'bc'));
-console.log(sameLastStr('sumo', 'omo'));
+// console.log(sameLastStr('sumo', 'omo'));
+
+
+
+//Best Practices  BUT it's not correct !!!
+function solution(str, ending) {
+    return str.endsWith(ending);
+}
+
+console.log(solution('sumo', 'omo'));
+// console.log(solution('abc', 'bc'));
+
