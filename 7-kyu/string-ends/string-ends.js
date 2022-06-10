5d783e1d6e47cab855f7ccf4d64e749a7aaa04da
Amir <amirhatam136@gmail.com>
2022-06-10 18:24:02 +0200

    String ends with, My solution

---
+++ 7-kyu/string-ends/string-ends.js
@@ -0,0 +1,7 @@
+//My Solution
+const sameLastStr = (a, b) => a.charAt(a.length - 1) === b.charAt(b.length - 1)
+
+
+
+// console.log(sameLastStr('abc', 'bc'));
+console.log(sameLastStr('sumo', 'omo'));
