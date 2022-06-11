66ae2e03c063c025290f98eeac634b91c56a6646
Amir <amirhatam136@gmail.com>
2022-06-11 21:08:15 +0200

    Pair of gloves, Best practices

--- 6-kyu/pair-gloves/pair-gloves.js
+++ 6-kyu/pair-gloves/pair-gloves.js
@@ -13,7 +13,27 @@ function numberOfPairs(gloves) {
     }
     return countPair
 }
+// console.log(numberOfPairs(["red", "green", "red", "blue", "blue", "blue", "blue"]));
 
 
-console.log(numberOfPairs(["red", "green", "red", "blue", "blue", "blue", "blue"]));
+//Best Practices
+function pairsNum(gloves) {
 
+    let pairs = 0
+        , counts = {};
+
+    for (let color of gloves) {
+
+        if (!counts.hasOwnProperty(color))
+            counts[color] = 0;
+
+        if (++counts[color] === 2) {
+            counts[color] -= 2;
+            pairs++;
+        }
+    }
+
+    return pairs;
+}
+
+console.log(pairsNum(["red", "green", "red", "blue", "blue", "blue", "blue"]));
