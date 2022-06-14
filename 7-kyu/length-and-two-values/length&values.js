70e0675db402aad94f9f696f8dd2c99f19096697
Amir <amirhatam136@gmail.com>
2022-06-14 16:01:30 +0200

    Length and two values, First Solution

--- 7-kyu/length-and-two-values/length&values.js
+++ 7-kyu/length-and-two-values/length&values.js
@@ -1,2 +1,14 @@
+// First Solution
+const alternate = (n, f, s) => {
+    let res = []
+    for (let i = 1; i <= n; i++) {
+        if (i % 2 === 0) {
+            res.push(s)
+        } else {
+            res.push(f)
+        }
+    }
+    return res
+}
 
-
+console.log(alternate(5, true, false));
