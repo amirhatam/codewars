b5974d7d9f61009bb733c4850751d8fc5e04cc41
Amir <amirhatam136@gmail.com>
2022-06-14 16:14:23 +0200

    Length and two values, Best practices

--- 7-kyu/length-and-two-values/length&values.js
+++ 7-kyu/length-and-two-values/length&values.js
@@ -11,4 +11,19 @@ const alternate = (n, f, s) => {
     return res
 }
 
-console.log(alternate(5, true, false));
+// console.log(alternate(5, true, false));
+
+//Best Practices
+function alternateB(n, firstValue, secondValue) {
+
+    return Array.from({ length: n }, (_, i) => {
+        if (i % 2 === 0)
+            return firstValue
+        else
+            return secondValue
+    });
+    //Short way
+    // return Array.from({length:n}, (_,i)=>i%2===0?firstValue:secondValue);
+}
+
+console.log(alternateB(5, true, false));
