7f8f83fd576bf9a49f4eaa02027c58b0b58e2d57
Amir <amirhatam136@gmail.com>
2022-07-16 20:57:11 +0200

    clear code

--- 8-kyu/beginner-series/2/clock.js
+++ 8-kyu/beginner-series/2/clock.js
@@ -1,7 +1,6 @@
 //Best practise
 const milliseconds = (h, m, s) => h * 60 * 60 * 1000 + m * 60 * 1000 + 1000 * s
 
-// console.log(milliseconds(0, 1, 30));
 
 //Another Best practise
 function past(h, m, s) {
