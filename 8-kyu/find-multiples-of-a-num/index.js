03a5fbb58805fe9ccb322e8b9bfe53f0ba9bb483
Amir <amirhatam136@gmail.com>
2022-07-19 16:59:04 +0200

    Find Multiples of a Number ,First Solution

---
+++ 8-kyu/find-multiples-of-a-num/index.js
@@ -0,0 +1,13 @@
+//First Solution
+const findMultiples = (integer, limit) => {
+    let res = []
+    for (let i = integer; i <= limit; i++) {
+        if (i % integer === 0) {
+            res.push(i)
+        }
+    }
+    return res
+}
+
+// findMultiples(2, 6)
+console.log(findMultiples(2, 6));
