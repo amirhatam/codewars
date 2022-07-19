e7d772bd5cc1e52a971286b803162474550852c2
Amir <amirhatam136@gmail.com>
2022-07-19 18:32:02 +0200

    Opposite number, Codewars users Best Practices

--- 8-kyu/opposite-number/index.js
+++ 8-kyu/opposite-number/index.js
@@ -3,4 +3,9 @@ const opposite = (n) => n < 0 ? Math.abs(n) : -Math.abs(n)
 
 
 
-console.log(opposite(20));
+// console.log(opposite(20));
+
+//Codewars users Best Practices 
+const oppositeNum = n => -n;
+
+console.log(oppositeNum(20));
