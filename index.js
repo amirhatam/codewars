bfb8035dbc82d0b11dd474bac0249888eddea8d6
Amir <amirhatam136@gmail.com>
2022-07-22 11:24:52 +0200

    Predict your age, First solution

---
+++ index.js
@@ -0,0 +1,15 @@
+// First Solution
+const getPredict = (age1, age2, age3, age4, age5, age6, age7, age8) => {
+    let arr = [age1, age2, age3, age4, age5, age6, age7, age8]
+
+    //Multiply each number by itself
+    let multiply = arr.map(e => e * e)
+
+    //Add them all together
+    let addAll = multiply.reduce((a, b) => a + b)
+
+    //Take the square root of the result & Divide by two
+    return Math.floor(Math.sqrt(addAll) / 2)
+}
+
+console.log(getPredict(65, 60, 75, 55, 60, 63, 64, 45));
