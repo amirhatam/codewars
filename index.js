62dccb7a5f7884652e2d8814a2bfefd6d3dc96d4
Amir <amirhatam136@gmail.com>
2022-07-22 11:31:09 +0200

    Predict your age, short way

--- index.js
+++ index.js
@@ -12,4 +12,13 @@ const getPredict = (age1, age2, age3, age4, age5, age6, age7, age8) => {
     return Math.floor(Math.sqrt(addAll) / 2)
 }
 
+// console.log(getPredict(65, 60, 75, 55, 60, 63, 64, 45));
+
+//Short Way
+const predictAge = (age1, age2, age3, age4, age5, age6, age7, age8) => {
+    let arr = [age1, age2, age3, age4, age5, age6, age7, age8]
+
+    return Math.floor(Math.sqrt(arr.map(e => e * e).reduce((a, b) => a + b)) / 2)
+}
+
 console.log(getPredict(65, 60, 75, 55, 60, 63, 64, 45));
